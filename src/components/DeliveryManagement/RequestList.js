70fa499186bb1531cc926ecd57df9905f06e2ca5
jambottle <jaewon.walks@gmail.com>
2022-02-19 19:25:04 +0900

    Feat: RequestList 컴포넌트 추가 기능 구현 - (1) 2계층 테이블을 데이터 연관성에 따라 분리 - 하나였던 2계층 테이블을 데이터 연관성에 따라 2개의 테이블로 나눠 데이터 스키마에 가깝게 레이아웃 변경

--- src/components/DeliveryManagement/RequestList.js
+++ src/components/DeliveryManagement/RequestList.js
@@ -23,28 +23,30 @@ import Typography from '@mui/material/Typography';
 
 function createOrderData(
   id,
-  number,
   status,
   productName,
+  productUnit,
   productAmount,
   deliveryAmount,
   isOutOfStock,
   recipientName,
-  recipientContact,
-  recipientAddress,
+  recipientContact1,
+  recipientAddress1,
+  recipientZipcode1,
   additionalInfo
 ) {
   return {
     id,
-    number,
     status,
     productName,
+    productUnit,
     productAmount,
     deliveryAmount,
     isOutOfStock,
     recipientName,
-    recipientContact,
-    recipientAddress,
+    recipientContact1,
+    recipientAddress1,
+    recipientZipcode1,
     additionalInfo,
   };
 }
@@ -68,15 +70,20 @@ function Row(props) {
         <TableCell component="th" scope="row">
           {row.id}
         </TableCell>
-        <TableCell>{row.number}</TableCell>
         <TableCell>{row.status}</TableCell>
         <TableCell>{row.productName}</TableCell>
+        <TableCell>{row.productUnit}</TableCell>
         <TableCell>{row.productAmount}</TableCell>
         <TableCell>{row.deliveryAmount}</TableCell>
         <TableCell>{row.isOutOfStock}</TableCell>
         <TableCell>{row.recipientName}</TableCell>
-        <TableCell>{row.recipientContact}</TableCell>
-        <TableCell sx={{ maxWidth: '250px' }}>{row.recipientAddress}</TableCell>
+        <TableCell>{row.recipientContact1}</TableCell>
+        <TableCell sx={{ maxWidth: '250px' }}>
+          {row.recipientAddress1}
+        </TableCell>
+        <TableCell sx={{ minWidth: '120px' }}>
+          {row.recipientZipcode1}
+        </TableCell>
       </TableRow>
 
       <TableRow>
@@ -87,43 +94,90 @@ function Row(props) {
                 variant="h6"
                 sx={{ my: 1, fontSize: '18px', fontWeight: '600' }}
               >
-                주문별 세부 정보
+                주문 관련 추가 정보
               </Typography>
 
-              <Table size="small" aria-label="purchases">
+              <Table size="small" aria-label="additional order info">
                 <TableHead>
                   <TableRow>
-                    <TableCell sx={{ maxWidth: '120px' }}>주문명</TableCell>
+                    <TableCell sx={{ width: '120px' }}>주문 번호</TableCell>
+                    <TableCell sx={{ width: '105px' }}>상품 ID</TableCell>
+                    <TableCell sx={{ width: '90px' }}>상품 코드</TableCell>
+                    <TableCell>주문명</TableCell>
                     <TableCell>주문서 양식</TableCell>
-                    <TableCell>상품 ID</TableCell>
-                    <TableCell>상품 코드</TableCell>
-                    <TableCell>주문 단위</TableCell>
-                    <TableCell>출고 창고명</TableCell>
-                    <TableCell>택배사명</TableCell>
-                    <TableCell>수취인 우편번호</TableCell>
-                    <TableCell sx={{ minWidth: '180px' }}>메모</TableCell>
+                    <TableCell>주문자 이름</TableCell>
+                    <TableCell>주문자 번호</TableCell>
+                    <TableCell>수량 변경</TableCell>
+                    <TableCell>출고 취소(상품)</TableCell>
+                    <TableCell>출고 취소(주문)</TableCell>
                   </TableRow>
                 </TableHead>
 
                 <TableBody>
                   {row.additionalInfo.map(info => (
-                    <TableRow key={info.productId}>
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        sx={{ maxWidth: '120px' }}
-                      >
-                        {info.name}
+                    <TableRow key={info.number}>
+                      <TableCell component="th" scope="row">
+                        {info.number}
                       </TableCell>
-                      <TableCell>{info.channel}</TableCell>
                       <TableCell>{info.productId}</TableCell>
                       <TableCell>{info.productCode}</TableCell>
-                      <TableCell>{info.productUnit}</TableCell>
+                      <TableCell>{info.name}</TableCell>
+                      <TableCell>{info.channel}</TableCell>
+                      <TableCell>{info.senderName}</TableCell>
+                      <TableCell>{info.senderContact}</TableCell>
+                      <TableCell>-</TableCell>
+                      <TableCell>-</TableCell>
+                      <TableCell>-</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+
+            <Box sx={{ mb: 2, p: 2 }}>
+              <Typography
+                variant="h6"
+                sx={{ mb: 1, fontSize: '18px', fontWeight: '600' }}
+              >
+                배송 관련 추가 정보
+              </Typography>
+
+              <Table size="small" aria-label="additional delivery info">
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ width: '120px' }}>주문 번호</TableCell>
+                    <TableCell sx={{ width: '105px' }}>상품 ID</TableCell>
+                    <TableCell sx={{ width: '90px' }}>상품 코드</TableCell>
+                    <TableCell>출고 창고명</TableCell>
+                    <TableCell>택배사명</TableCell>
+                    <TableCell>수취인 번호 2</TableCell>
+                    <TableCell sx={{ maxWidth: '250px' }}>
+                      수취인 주소 2
+                    </TableCell>
+                    <TableCell sx={{ minWidth: '125px' }}>우편번호 2</TableCell>
+                    <TableCell sx={{ minWidth: '125px' }}>메모</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {row.additionalInfo.map(info => (
+                    <TableRow key={info.number}>
+                      <TableCell component="th" scope="row">
+                        {info.number}
+                      </TableCell>
+                      <TableCell>{info.productId}</TableCell>
+                      <TableCell>{info.productCode}</TableCell>
                       <TableCell>{info.deliveryStorage}</TableCell>
                       <TableCell>{info.deliveryCompany}</TableCell>
-                      <TableCell>{info.recipientZipcode}</TableCell>
-                      <TableCell sx={{ minWidth: '180px' }}>
-                        {info.deliveryMemo}
+                      <TableCell>{row.recipientContact2}</TableCell>
+                      <TableCell sx={{ maxWidth: '250px' }}>
+                        {row.recipientAddress2}
+                      </TableCell>
+                      <TableCell sx={{ minWidth: '125px' }}>
+                        {row.recipientZipcode2}
+                      </TableCell>
+                      <TableCell sx={{ minWidth: '125px' }}>
+                        배송 메시지: {info.deliveryMemo}
                       </TableCell>
                     </TableRow>
                   ))}
@@ -140,28 +194,31 @@ function Row(props) {
 Row.propTypes = {
   row: PropTypes.shape({
     id: PropTypes.number.isRequired,
-    number: PropTypes.string.isRequired,
     status: PropTypes.string.isRequired,
     productName: PropTypes.string.isRequired,
+    productUnit: PropTypes.number.isRequired,
     productAmount: PropTypes.number.isRequired,
     deliveryAmount: PropTypes.number.isRequired,
     isOutOfStock: PropTypes.string.isRequired,
     recipientName: PropTypes.string.isRequired,
-    recipientContact: PropTypes.string.isRequired,
-    recipientAddress: PropTypes.string.isRequired,
+    recipientContact1: PropTypes.string.isRequired,
+    recipientAddress1: PropTypes.string.isRequired,
+    recipientZipcode1: PropTypes.number,
     additionalInfo: PropTypes.arrayOf(
       PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        channel: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
         productId: PropTypes.string.isRequired,
         productCode: PropTypes.number.isRequired,
-        productUnit: PropTypes.number.isRequired,
-        deliveryStorage: PropTypes.string.isRequired,
-        deliveryCompany: PropTypes.string,
-        deliveryMemo: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        channel: PropTypes.string.isRequired,
         senderName: PropTypes.string,
         senderContact: PropTypes.string,
-        recipientZipcode: PropTypes.number,
+        deliveryStorage: PropTypes.string.isRequired,
+        deliveryCompany: PropTypes.string,
+        recipientContact2: PropTypes.string,
+        recipientAddress2: PropTypes.string,
+        recipientZipcode2: PropTypes.number,
+        deliveryMemo: PropTypes.string,
       })
     ),
   }),
@@ -170,264 +227,276 @@ Row.propTypes = {
 const ROWS = [
   createOrderData(
     1,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     2,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     3,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     4,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     5,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     6,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     7,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     8,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     9,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     10,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     11,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
   ),
   createOrderData(
     12,
-    '0000921944385',
     '주문명 세트 지정',
     '에이스 크래커 150g',
+    1,
     1,
     1,
     '출고 주문 대기',
     '김재원',
     '010-0000-0000',
     '서울특별시 서대문구 연세로 50, 현대 아이파크 오피스텔 505동 5005호',
+    '03722',
     [
       {
-        name: '[특별할인] 해태과자 선물세트 1세트',
-        channel: '일반 온라인 판매',
+        number: '0000921944385',
         productId: 'DNLQ00051',
         productCode: 19071,
-        productUnit: 1,
+        name: '[특별할인] 해태과자 선물세트 1세트',
+        channel: '일반 온라인 판매',
         deliveryStorage: '(창고 미지정)',
       },
     ]
@@ -456,15 +525,16 @@ export default function RequestList() {
               <TableRow>
                 <TableCell />
                 <TableCell>순번</TableCell>
-                <TableCell>주문 번호</TableCell>
                 <TableCell>매핑 상태</TableCell>
                 <TableCell>상품명</TableCell>
+                <TableCell>주문 단위</TableCell>
                 <TableCell>주문 수량</TableCell>
                 <TableCell>출고 수량</TableCell>
                 <TableCell>재고 부족 여부</TableCell>
                 <TableCell>수취인</TableCell>
-                <TableCell>수취인 번호</TableCell>
-                <TableCell sx={{ maxWidth: '250px' }}>수취인 주소</TableCell>
+                <TableCell>수취인 번호 1</TableCell>
+                <TableCell sx={{ maxWidth: '250px' }}>수취인 주소 1</TableCell>
+                <TableCell sx={{ minWidth: '125px' }}>우편번호 1</TableCell>
               </TableRow>
             </TableHead>
 
